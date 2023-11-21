8b73aa319da3fc5eaf2eb8aeec1c2674c045a068
Leon Bojic <leonbojic@proton.me>
2023-11-21 16:38:26 +0100

    Initialize spring boot project

---
+++ src/main/java/leonbojic/shoppinglistserver/ShoppingListServerApplication.java
@@ -0,0 +1,13 @@
+package leonbojic.shoppinglistserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListServerApplication.class, args);
+	}
+
+}
